2777bda232161b9bc751a123417916cc9736c19a
KuraoHikari <dewaindra705@gmail.com>
2024-05-22 19:59:07 +0800

    chore: fix register schema

--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -1,12 +1,14 @@
 import { z } from "zod";
 
 export const registrationSchema = z.object({
-  username: z.string(),
-  email: z.string().email(),
-  password: z.string().min(8),
+ username: z.string().min(2, {
+  message: "Username must be at least 2 characters.",
+ }),
+ email: z.string().email(),
+ password: z.string().min(8),
 });
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+ email: z.string().email(),
+ password: z.string().min(8),
 });
