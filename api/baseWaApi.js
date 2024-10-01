9030441038c1e1531e56d41798b7613016fcfb9c
KuraoHikari <dewaindra705@gmail.com>
2024-10-01 19:27:15 +0800

    chore: test proxy

--- api/baseWaApi.js
+++ api/baseWaApi.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 const baseUrl = process.env.WA_WEB_API;
 
 const kyInstance = ky.create({
-  prefixUrl: baseUrl,
+  prefixUrl: "http://202.73.25.70:54432",
   parseJson: (text) =>
     JSON.parse(text, (key, value) => {
       if (key.endsWith("At")) return new Date(value);
